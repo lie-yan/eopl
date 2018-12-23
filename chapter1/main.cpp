30c63c07fed5629b7d5947bc5285062a598ca87a
Lie Yan <robyn.lie.yan@outlook.com>
2018-12-23 19:15:02 +0800

    minor change

--- chapter1/main.cpp
+++ chapter1/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <functional>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
 int main () {
-  std::cout << "Hello, World!" << std::endl;
+  std::vector<int> v = {1, 2, 3, 4};
+  auto sum = std::accumulate(std::begin(v),
+                             std::end(v),
+                             1,
+                             std::multiplies<>());
+  std::cout << sum << std::endl;
   return 0;
 }
