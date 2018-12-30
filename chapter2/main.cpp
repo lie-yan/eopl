cccbd5ab017c0ed197b4458c6fce153ddbbbd2d0
Lie Yan <robyn.lie.yan@outlook.com>
2018-12-30 19:04:48 +0800

    modify main.cpp

--- chapter2/main.cpp
+++ chapter2/main.cpp
@@ -8,10 +8,13 @@ int main () {
 
   std::cout << "sizeof(eopl::Value): " << sizeof(eopl::Value) << std::endl;
 
-  Value v = ValueList{{false, 2, 3.0, Symbol("w"s)}};
+  Value v = ValueList{{false, 1, 2.0, String("three"s), Symbol("four"s), ValueList{}}};
+
   std::cout << v.index() << std::endl;
-  ValueList& vlist = std::get<5>(v).get();
-  for (auto& x : vlist) {
+  if (v.index() != 5) return 0;
+
+  const ValueList& vlist = std::get<5>(v).get();
+  for (const auto& x : vlist) {
     std::cout << x.index() << std::endl;
   }
 }
