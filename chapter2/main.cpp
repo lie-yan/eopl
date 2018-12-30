437f3b491e13b619b4a96946ac8991d7c0905325
Lie Yan <robyn.lie.yan@outlook.com>
2018-12-30 16:34:20 +0800

    chapter2: play with variant

---
+++ chapter2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "scope_support.h"
+
+int main () {
+  std::cout << "Hello, World!" << std::endl;
+
+  eopl::Value v = eopl::Vector{{1,2,3}};
+
+
+  return 0;
+}
---
+++ chapter2/scope_support.h
@@ -0,0 +1,25 @@
+//
+// Created by robin on 2018-12-30.
+//
+
+#pragma once
+
+#include <string>
+#include <variant>
+#include <boost/variant.hpp>
+
+namespace eopl {
+
+using Variable = std::string;
+
+using Value = std::variant<bool,
+                           int,
+                           double,
+                           boost::recursive_wrapper<std::string>,
+                           boost::recursive_wrapper<struct Vector>>;
+
+struct Vector {
+  std::vector<Value> data;
+};
+
+}
