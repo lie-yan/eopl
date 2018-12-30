a5fc02ec011aa95b4dff023004363c88e80c34e7
Lie Yan <robyn.lie.yan@outlook.com>
2018-12-30 19:28:57 +0800

    using aliases for recursive_wrapper

--- chapter2/main.cpp
+++ chapter2/main.cpp
@@ -4,16 +4,15 @@
 int main () {
   using namespace std::string_literals;
   using eopl::Symbol, eopl::String, eopl::Value, eopl::ValueList;
+  using eopl::RwValueList;
 
   std::cout << "sizeof(eopl::Value): " << sizeof(eopl::Value) << std::endl;
 
-  Value v = ValueList{{false, 1, 2.0, String("three"s), Symbol("four"s), ValueList{}}};
+  Value v = ValueList{false, 1, 2.0, String("three"s), Symbol("four"s), ValueList{}};
 
   std::cout << v.index() << std::endl;
-  const int index_vlist = 5;
-  if (v.index() != index_vlist) return 0;
-
-  const ValueList& vlist = std::get<index_vlist>(v).get();
+  
+  const ValueList& vlist = std::get<RwValueList>(v).get();
   for (const auto& x : vlist) {
     std::cout << x.index() << std::endl;
   }
--- chapter2/scope_support.h
+++ chapter2/scope_support.h
@@ -13,13 +13,17 @@ namespace eopl {
 struct Symbol : std::string { using std::string::string; };
 struct String : std::string { using std::string::string; };
 
+using RwString = boost::recursive_wrapper<String>;
+using RwSymbol = boost::recursive_wrapper<Symbol>;
+using RwValueList = boost::recursive_wrapper<struct ValueList>;
+
 using Value = std::variant<bool,
                            int,
                            double,
-                           boost::recursive_wrapper<String>,
-                           boost::recursive_wrapper<Symbol>,
-                           boost::recursive_wrapper<struct ValueList>>;
+                           RwString,
+                           RwSymbol,
+                           RwValueList>;
 
-struct ValueList : std::vector<Value> { };
+struct ValueList : std::vector<Value> { using std::vector<Value>::vector; };
 
 }
