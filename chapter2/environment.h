a5df1cfe30fe130b8f8a635db9276bf46178a6c1
Lie Yan <robyn.lie.yan@outlook.com>
2019-01-01 14:04:00 +0800

    implement environment

--- chapter2/environment.h
+++ chapter2/environment.h
@@ -1,14 +1,39 @@
+#include <utility>
+
 //
 // Created by robin on 2019-01-01.
 //
 
 #pragma once
 
+#include <memory>
+#include "value.h"
+
 namespace eopl {
 
-class Environment {
+class Environment : public std::enable_shared_from_this<Environment> {
 public:
+  using SpEnv = std::shared_ptr<Environment>;
+
+  struct SymbolNotFoundError : std::runtime_error {
+    using std::runtime_error::runtime_error;
+  };
+
   Environment () = default;
+  Environment (const Environment&) = delete;
+  Environment& operator = (const Environment&) = delete;
+  Environment (Environment&&) = delete;
+  Environment& operator = (Environment&&) = delete;
+
+  Environment (SpEnv parent, std::pair<Symbol, Value> pair);
+
+  static SpEnv make_empty ();
+  const Value& apply (const Symbol& sym);
+  SpEnv extend (Symbol sym, Value value);
+
+private:
+  SpEnv parent_;
+  std::pair<Symbol, Value> pair_;
 };
 
 }
--- chapter2/main.cpp
+++ chapter2/main.cpp
@@ -5,6 +5,17 @@
 int main () {
   using namespace eopl;
 
-  Environment env;
   std::cout << "sizeof(Environment): " << sizeof(Environment) << std::endl;
+
+  Environment::SpEnv env = Environment::make_empty();
+
+  auto x = Symbol("x");
+  auto env1 = env->extend(x, 1);
+  std::cout << x << ": " << env1->apply(x) << std::endl;
+
+  try {
+    env1->apply(Symbol("y"));
+  } catch (const Environment::SymbolNotFoundError& e) {
+    std::cout << e.what() << std::endl;
+  }
 }
