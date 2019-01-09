18d0c0b45bb754a902e569be546d0341c4290e31
Lie Yan <robyn.lie.yan@outlook.com>
2019-01-10 07:48:31 +0800

    simplify the output format of Expression

--- chapter3/let/src/expr.cpp
+++ chapter3/let/src/expr.cpp
@@ -25,32 +25,32 @@ std::ostream& operator << (std::ostream& os, const Expression& exp) {
   return os;
 }
 std::ostream& operator << (std::ostream& os, const ConstExp& constExp) {
-  os << "ConstExp(num: " << constExp.num << ")";
+  os << "ConstExp(" << constExp.num << ")";
   return os;
 }
 std::ostream& operator << (std::ostream& os, const VarExp& varExp) {
-  os << "VarExp(var: " << varExp.var << ")";
+  os << "VarExp(" << varExp.var << ")";
   return os;
 }
 std::ostream& operator << (std::ostream& os, const DiffExp& diffExp) {
-  os << "DiffExp(exp1: " << diffExp.exp1 << ", exp2: " << diffExp.exp2 << ")";
+  os << "DiffExp(" << diffExp.exp1 << ", " << diffExp.exp2 << ")";
   return os;
 }
 std::ostream& operator << (std::ostream& os, const ZeroQExp& qExp) {
-  os << "ZeroQExp(exp1: " << qExp.exp1 << ")";
+  os << "ZeroQExp(" << qExp.exp1 << ")";
   return os;
 }
 std::ostream& operator << (std::ostream& os, const IfExp& ifExp) {
-  os << "IfExp(exp1: " << ifExp.exp1 << ", exp2: " << ifExp.exp2 << ", exp3: " << ifExp.exp3 << ")";
+  os << "IfExp(" << ifExp.exp1 << ", " << ifExp.exp2 << ", " << ifExp.exp3 << ")";
   return os;
 }
 std::ostream& operator << (std::ostream& os, const LetExp& letExp) {
-  os << "LetExp(var: " << letExp.var << ", exp1: " << letExp.exp1 << ", body: " << letExp.body << ")";
+  os << "LetExp(" << letExp.var << ", " << letExp.exp1 << ", " << letExp.body << ")";
   return os;
 }
 
 std::ostream& operator << (std::ostream& os, const Program& program) {
-  os << "Program(exp1: " << program.exp1 << ")";
+  os << "Program(" << program.exp1 << ")";
   return os;
 }
 }
