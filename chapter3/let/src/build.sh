0d44e16733f62834137e921096be526bdd8b925e
Lie Yan <robyn.lie.yan@outlook.com>
2019-01-09 22:59:50 +0800

    build with CMake

--- chapter3/let/src/build.sh
+++ chapter3/let/src/build.sh
@@ -2,7 +2,7 @@
 
 reflex --flex --bison-cc --bison-locations \
   --namespace=yy --lexer=Lexer --lex=yylex \
-  --header-file --interactive scanner.l
+  --header-file=lex.yy.h --interactive scanner.l
 bison -d parser.y
 
 # c++ -std=c++17 main.cpp lex.yy.cpp parser.tab.cc -lreflex
