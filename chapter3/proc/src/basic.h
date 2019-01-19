4886ce68b7b42da506e9e9cc13831d493b4ec24f
Lie Yan <robyn.lie.yan@outlook.com>
2019-01-19 21:25:13 +0800

    minor change

--- chapter3/proc/src/basic.h
+++ chapter3/proc/src/basic.h
@@ -6,14 +6,26 @@
 
 namespace eopl {
 
+/**
+ * @brief Apply function `f` on the elements of range [first, last)
+ *        successively. Between consecutive calls of `f`, function `g`
+ *        is called.
+ * @param first the start of range
+ * @param last the end of range
+ * @param f a unary function
+ * @param g a function that accepts no argument
+ */
 template<typename InputIt, typename F, typename G>
 void interleave (InputIt first, InputIt last, F f, G g) {
-  if (first == last) return;
-
-  f(*first++);
-  while (first != last) {
-    g();
+  
+  if (first == last) {
+    return;
+  } else {
     f(*first++);
+    while (first != last) {
+      g();
+      f(*first++);
+    }
   }
 }
 
