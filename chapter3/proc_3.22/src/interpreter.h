2ea09ef9f3f195c789a8e4bdf9e2ca2c36f0aebd
Lie Yan <robyn.lie.yan@outlook.com>
2019-01-21 21:43:18 +0800

    initial code for exercise 3.22

---
+++ chapter3/proc_3.22/src/interpreter.h
@@ -0,0 +1,31 @@
+//
+// Created by robin on 2019-01-10.
+//
+
+#pragma once
+
+#include "value.h"
+#include "expr.h"
+
+namespace eopl {
+
+//using Env = Environment<Symbol, Value>;
+//using SpEnv = Env::SpEnv;
+
+Value value_of (const Program& program, SpEnv env);
+Value value_of (const Expression& exp, SpEnv env);
+Value value_of (const ConstExp& exp, SpEnv env);
+Value value_of (const VarExp& exp, SpEnv env);
+Value value_of (const IfExp& exp, SpEnv env);
+Value value_of (const LetExp& exp, SpEnv env);
+Value value_of (const CondExp& exp, SpEnv env);
+Value value_of (const UnpackExp& exp, SpEnv env);
+Value value_of (const ProcExp& exp, SpEnv env);
+Value value_of (const CallExp& exp, SpEnv env);
+
+std::vector<Value> value_of (const std::vector<Expression>& exps, SpEnv env);
+
+SpEnv make_initial_env ();
+Value eval (const std::string& s);
+
+}
