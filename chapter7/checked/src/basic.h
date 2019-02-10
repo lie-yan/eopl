a337395c9acb66431da423023330958f4aab5354
Lie Yan <robyn.lie.yan@outlook.com>
2019-02-10 11:57:53 +0800

    add initial code for checked

---
+++ chapter7/checked/src/basic.h
@@ -0,0 +1,32 @@
+//
+// Created by robin on 2018-12-30.
+//
+
+#pragma once
+
+namespace eopl {
+
+/**
+ * @brief Apply function <code>f</code> on the elements of range
+ *        <code>[first, last)</code> successively. Between consecutive calls to
+ *        <code>f</code>, invoke function <code>g</code>.
+ * @param first the start of range
+ * @param last the end of range
+ * @param f a unary function
+ * @param g a function that accepts no argument
+ */
+template<typename InputIt, typename F, typename G>
+void interleave (InputIt first, InputIt last, F f, G g) {
+
+  if (first == last) {
+    return;
+  } else {
+    f(*first++);
+    while (first != last) {
+      g();
+      f(*first++);
+    }
+  }
+}
+
+}
